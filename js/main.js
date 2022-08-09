8dce073c23c40af5911148853b818f8a9ab381a2
Dayane Rodrigues <dayanealrodrigues@gmail.com>
2022-08-09 18:34:42 -0300

    salvando os dados na tela

--- js/main.js
+++ js/main.js
@@ -1,39 +1,45 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
-const itens = localStorage.getItem('itens') || [];
+const itens = JSON.parse(localStorage.getItem('itens')) || [];
 
-console.log(itens)
+itens.forEach( (elemento) => {
+    criarTarefa(elemento)
+})
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
     const nome = evento.target.elements['nome'];
-    const quantidade = evento.target.elements['quantidade']
-    criarTarefa(nome.value,quantidade.value);
+    const quantidade = evento.target.elements['quantidade'] 
+    
+    // criar objeto para formar o conjunto de chave valor
+    const itemAtual = {
+        "nome" : nome.value,
+        "quantidade": quantidade.value
+    }
+
+    criarTarefa(itemAtual);
+
+    // atualizar o array com o item atual
+    itens.push(itemAtual)
+
+    // salvar dados no localStorage
+    localStorage.setItem("itens", JSON.stringify(itens));
+    
     nome.value = "";
     quantidade.value = "";
 })
 
-function criarTarefa (nome, quantidade){
+function criarTarefa (item){
     const novaTarefa = document.createElement('li');
    // novaTarefa.classlist.add('item');
 
     const numeroDeItem = document.createElement('strong');
-    numeroDeItem.innerHTML = quantidade;
+    numeroDeItem.innerHTML = item.quantidade;
 
     novaTarefa.appendChild(numeroDeItem);
-    novaTarefa.innerHTML +=nome;
+    novaTarefa.innerHTML += item.nome;
 
     lista.appendChild(novaTarefa);
 
-    // criar objeto para formar o conjunto de chave valor
-    const itemAtual = {
-        "nome" : nome,
-        "quantidade": quantidade
-    }
-    
-    // atualizar o array com o item atual
-    itens.push(itemAtual)
-    // salvar dados no localStorage
-
-    localStorage.setItem("item", JSON.stringify(itens));
+   
 }
